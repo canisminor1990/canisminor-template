a8930223fdd59edc51f23d947ac1f2c34eb94c15
倏昱 <yufan.yyf@antgroup.com>
2023-05-08 17:30:22 +0800

    🔨 chore: switch pnpm

--- .umirc.ts
+++ .umirc.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   title: 'Template - CanisMinor',
   favicons: ['https://gw.alipayobjects.com/zos/bmw-prod/51a51720-8a30-4430-b6c9-be5712364f04.svg'],
   routes: [{ path: '/', component: 'index' }],
+  npmClient: 'pnpm',
   define: {
     'process.env': process.env,
   },
